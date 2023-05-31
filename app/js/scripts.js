eddc9b0766f80737b69f46ee50465369d8d55e4f
David Im <dji216@nyu.edu>
2023-05-30 20:21:19 -0400

    added close button

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -8,10 +8,35 @@
 //   e.preventDefault();
 // }
 
-document.addEventListener("click", show);
+// document.addEventListener("click", show);
 
-function show(e) {
-  console.log(e.target);
-  // 'event.target' is the clicked element
-  e.preventDefault();
+// // function show(e) {
+// //   console.log(e.target);
+// //   // 'event.target' is the clicked element
+// //   e.preventDefault();
+// // }
+
+
+// document.addEventListener("click", handleClicks);
+// var body = document.querySelector("body");
+
+// function handleClicks(e) {
+//   if (e.target.matches(".map")) {
+//     body.classList.add("showme");
+//     e.preventDefault();
+//   } else {
+//     body.classList.remove("showme");
+//   }
+// }
+
+var body = document.querySelector("body");
+document.addEventListener("click", handleClicks);
+
+function handleClicks(e) {
+  if (e.target.matches(".map") || e.target.matches(".closer")) {
+    body.classList.toggle("showme");
+    e.preventDefault();
+  } else {
+    body.classList.remove("showme");
+  }
 }
